7bc182aae469609c489c6778ec972d9a65f63f0a
David Bitner <bitner@dbspatial.com>
2015-02-23 13:55:41 -0600

    sync

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 cd /var/www/id
 cp openstreetmap.us/iD/release/index.html .
 wget -m -k http://openstreetmap.us/iD/release
+wget -m -k http://openstreetmap.us/iD/release/land.html
 wget -m -k http://openstreetmap.us/iD/release/img/pattern/wetland.png
 wget -m -k http://openstreetmap.us/iD/release/img/pattern/construction.png
 wget -m -k http://openstreetmap.us/iD/release/img/pattern/orchard.png
@@ -10,4 +11,4 @@ wget -m -k http://openstreetmap.us/iD/release/img/pattern/farmland.png
 wget -m -k http://openstreetmap.us/iD/release/img/pattern/dots.png
 wget -m -k http://openstreetmap.us/iD/release/img/loader-black.gif
 wget -m -k http://openstreetmap.us/iD/release/img/bing_maps.png
-cp index.html openstreetmap.us/iD/release/
+cp index.html openstreetmap.us/iD/release/index.html
