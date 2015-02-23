c1d77e452b7c2a9659c0566e957c5ba17b991245
David Bitner <bitner@dbspatial.com>
2015-02-23 14:30:41 -0600

    fix sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /var/www/id
-cp openstreetmap.us/iD/release/index.html .
 wget -m -k http://openstreetmap.us/iD/release
 wget -m -k http://openstreetmap.us/iD/release/land.html
 wget -m -k http://openstreetmap.us/iD/release/img/pattern/wetland.png
